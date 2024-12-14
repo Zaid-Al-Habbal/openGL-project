5e0681a25fc8234763591fa4fb758bd86028117e
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2024-12-14 14:00:14 +0300

    add MSAA

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -26,6 +26,9 @@ void Controller::initializeOpenGLSettings(){
 
     // configure global opengl state
     glEnable(GL_DEPTH_TEST);
+    // MSAA (Multiple sub-sample anti-analysing)
+    glEnable(GL_MULTISAMPLE); // enabled by default on some drivers, but not all so always enable to make sure
+
 
     // enable blending:
     glEnable(GL_BLEND);
@@ -48,6 +51,8 @@ bool Controller::initializeWindow(const std::string& title) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);  // Request 4x MSAA
 
     window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, title.c_str(), nullptr, nullptr);
     if (!window) {
